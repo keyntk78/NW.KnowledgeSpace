5e94f3c850768d8c654b1c74817814f66f83f9ef
Nguyễn Tuấn Kiệt <101924064+keyntk78@users.noreply.github.com>
2024-02-04 04:07:57 +0700

    Api Function

---
+++ src/NW.KnowledgeSpace.ViewModel/Systems/Function/FunctionCreateRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NW.KnowledgeSpace.ViewModel.Systems.Function
+{
+    public class FunctionCreateRequest
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Url { get; set; }
+
+        public int SortOrder { get; set; }
+
+        public string ParentId { get; set; }
+    }
+}
