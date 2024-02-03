fd407e72c5b3e6090c4010c88271198bd16f9a2c
Nguyễn Tuấn Kiệt <101924064+keyntk78@users.noreply.github.com>
2024-02-04 04:13:10 +0700

    Unit Test function

---
+++ test/NW.KnowledgeSpace.Backend.UnitTest/InMemoryDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NW.KnowledgeSpace.Backend.Data;
+
+namespace NW.KnowledgeSpace.Backend.UnitTest
+{
+    public class InMemoryDbContextFactory
+    {
+        public ApplicationDbContext GetApplicationDbContext()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "InMemoryApplicationDatabase")
+                .Options;
+            var dbContext = new ApplicationDbContext(options);
+
+            return dbContext;
+        }
+    }
+}
