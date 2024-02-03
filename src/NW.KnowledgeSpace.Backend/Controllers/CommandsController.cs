c53d1ad2655fc574659a3d1cea0af4b076a1b42c
Nguyễn Tuấn Kiệt <101924064+keyntk78@users.noreply.github.com>
2024-02-04 04:24:11 +0700

    Command API

---
+++ src/NW.KnowledgeSpace.Backend/Controllers/CommandsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NW.KnowledgeSpace.Backend.Data;
+using NW.KnowledgeSpace.ViewModel.Systems.Command;
+
+namespace NW.KnowledgeSpace.Backend.Controllers
+{
+
+    public class CommandsController : BaseController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CommandsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> GetCommants()
+        {
+            var commands = _context.Commands;
+
+            var commandVms = await commands.Select(u => new CommandVm()
+            {
+                Id = u.Id,
+                Name = u.Name,
+            }).ToListAsync();
+
+            return Ok(commandVms);
+        }
+    }
+}
---
+++ src/NW.KnowledgeSpace.ViewModel/Systems/Command/AddCommandToFunctionRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NW.KnowledgeSpace.ViewModel.Systems.Command
+{
+    public class AddCommandToFunctionRequest
+    {
+        public string CommandId { get; set; }
+
+        public string FunctionId { get; set; }
+    }
+}
